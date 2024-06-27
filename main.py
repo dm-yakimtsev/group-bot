a3693c43e4dd0a163c3c12fb2688a5418bdbe239
dm-yakimtsev <batyayakim08@gmail.com>
2024-06-27 14:58:47 +0500

    Сделал телеграм бота

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('HELLO WORLD!')
+from telegram import Update
+from telegram.ext import Application, MessageHandler, filters, CallbackContext
+from config import TOKEN
+
+
+async def start(update: Update, context: CallbackContext) -> None:
+    await update.message.reply_text('Привет!')
+
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(MessageHandler(filters.ALL, start))
+    app.run_polling()
+
+
+if __name__ == '__main__':
+    main()
